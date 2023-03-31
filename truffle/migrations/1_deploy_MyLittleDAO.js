94446a170828f4c65c205754840f48f561341602
LEL <lel@example.com>
2023-03-31 11:21:08 +0200

    Add contract modifications to deploiement

--- truffle/migrations/1_deploy_MyLittleDAO.js
+++ truffle/migrations/1_deploy_MyLittleDAO.js
@@ -1,5 +1,12 @@
 const MyLittleDAO = artifacts.require("MyLittleDAO");
 
-module.exports = function (deployer) {
-  deployer.deploy(MyLittleDAO);
+module.exports = async function (deployer) {
+  await deployer.deploy(MyLittleDAO);
+  const instance = await MyLittleDAO.deployed();
+  const oldMaxVoteSession = await instance.maxVoteSession();
+  await instance.setMaxVoteSession(10001);
+  const newMaxVoteSession = await instance.maxVoteSession();
+
+  console.log("Old max session: " + oldMaxVoteSession.toString() + " New max session : " + newMaxVoteSession.toString());
+
 };
