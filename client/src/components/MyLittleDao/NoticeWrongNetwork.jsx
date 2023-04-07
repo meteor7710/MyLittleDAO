f489aec207f9176e083bb0ddf4994c7ab26a7705
LEL <lel@example.com>
2023-04-07 20:54:06 +0200

    add NoticeWrongNetwork NoticeNoArtifact

--- client/src/components/MyLittleDao/NoticeWrongNetwork.jsx
+++ client/src/components/MyLittleDao/NoticeWrongNetwork.jsx
@@ -1,9 +1,13 @@
+import { Box, Alert, AlertIcon } from '@chakra-ui/react';
+
 function NoticeWrongNetwork() {
-  return (
-    <p>
-      ⚠️ MetaMask is not connected to the same network as the one you deployed to.
-    </p>
-  );
+    return (
+        <section className="Votes">
+            <Box my="10px" p="25px" border='1px' borderRadius='25px' borderColor='gray.200'>
+                <Alert width="auto" my="25px" status='warning' borderRadius='25px'> <AlertIcon />MetaMask is not connected to the correct network.</Alert>
+            </Box>
+        </section>
+    );
 }
 
 export default NoticeWrongNetwork;
--- client/src/components/MyLittleDao/index.jsx
+++ client/src/components/MyLittleDao/index.jsx
@@ -1,17 +1,20 @@
 import useEth from "../../contexts/EthContext/useEth";
-import NoticeNoArtifact from ".//NoticeNoArtifact";
+import NoticeNoArtifact from "./NoticeNoArtifact";
 import NoticeWrongNetwork from "./NoticeWrongNetwork";
+import { Container } from '@chakra-ui/react';
 
 function MyLittleDAO() {
   const { state } = useEth();
 
   return (
     <div className="MyLittleDAO">
+      <Container maxW="4xl">
       {
         !state.artifact ? <NoticeNoArtifact /> :
           !state.contract ? <NoticeWrongNetwork /> :
             <span> HOME </span>
       }
+      </Container>
     </div>
   );
 }
