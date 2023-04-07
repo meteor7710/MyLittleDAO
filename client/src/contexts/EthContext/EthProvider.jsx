3835828def694299c9f04f3512df6ff5650fa8ef
LEL <lel@example.com>
2023-04-07 18:09:32 +0200

    Add header

--- client/src/contexts/EthContext/EthProvider.jsx
+++ client/src/contexts/EthContext/EthProvider.jsx
@@ -1,10 +1,13 @@
-import React, { useReducer, useCallback, useEffect } from "react";
+import React, { useReducer, useCallback, useEffect, useState } from "react";
 import Web3 from "web3";
 import EthContext from "./EthContext";
 import { reducer, actions, initialState } from "./state";
+import { Button, Box, Flex, Heading, Text, Image } from '@chakra-ui/react';
+
 
 function EthProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [account, setAccount] = useState("");
 
   const init = useCallback(
     async artifact => {
@@ -24,6 +27,7 @@ function EthProvider({ children }) {
           type: actions.init,
           data: { artifact, web3, accounts, networkID, contract }
         });
+        setAccount(accounts[0]);
       }
     }, []);
 
@@ -52,11 +56,35 @@ function EthProvider({ children }) {
     };
   }, [init, state.artifact]);
 
+  const connectToMetaMask = async () => {
+    try {
+      await window.ethereum.enable();
+      const artifact = require("../../contracts/MyLittleDAO.json");
+      init(artifact);
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
   return (
     <EthContext.Provider value={{
       state,
       dispatch
-    }}>
+    }}> 
+      <header>
+        <Box p="10px" background="gray.200">
+        <Flex justifyContent="space-between" alignItems="center" width="100%">
+            <Box width="30%"><Image src='https://i.seadn.io/gcs/files/e7b82ed7b42b2eabf32d463fe5d361bd.png?auto=format&w=1000' alt='Icone' borderRadius='full'  boxSize='75px' /></Box>
+            <Heading as='h1' size='lg'>MyLittleDAO</Heading>
+            {
+            account ? <Text width="30%" ></Text> :
+              <Button width="30%" colorScheme="orange" onClick={connectToMetaMask}>Connect to MetaMask</Button>
+          }
+        </Flex>
+        </Box>
+      </header>
       {children}
     </EthContext.Provider>
   );
