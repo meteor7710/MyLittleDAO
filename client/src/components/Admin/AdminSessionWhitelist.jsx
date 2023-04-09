46fa566bf6b9fd4c57a74bd6232a9081700a2a12
LEL <lel@example.com>
2023-04-09 11:18:00 +0200

    Fix whitelist voter removal

--- client/src/components/Admin/AdminSessionWhitelist.jsx
+++ client/src/components/Admin/AdminSessionWhitelist.jsx
@@ -8,15 +8,21 @@ import {
 
 function AdminSessionWhitelist({ sessionSelected, addressToWhitelistLog, setAddressToWhitelistLog }) {
 
-    const [addressToWhitelist, setAddressToWhitelist] = useState(""); 
+    const [addressToWhitelist, setAddressToWhitelist] = useState("");
+    const [addressToRemove, setAddressToRemove] = useState("");
     const [registeredAddresses, setRegisteredAddresses] = useState();
     const [errorMsg, setErrorMsg] = useState("");
     const { state: { contract, accounts, web3, creationBlock } } = useEth();
     const { isOpen, onOpen, onClose } = useDisclosure()
 
     //Manage address input
-    const handleAdressChange = e => {
+    const handleAddressChange = e => {
         setAddressToWhitelist(e.target.value);
+    };
+
+    //Manage address input
+    const handleRemoveChange = e => {
+        setAddressToRemove(e.target.value);
     };
 
     //Add address to whitelist
@@ -31,7 +37,7 @@ function AdminSessionWhitelist({ sessionSelected, addressToWhitelistLog, setAddr
 
         //Validation max voter not reach
         const maxVoterperSession = await contract.methods.maxVoterperSession().call({ from: accounts[0] });
-        if ( (voterRegisteredEvents.length - voterUnregisteredEvents.length) >= maxVoterperSession ) { setErrorMsg("Max voter per session reached"); onOpen(); setAddressToWhitelist(""); return; }
+        if ((voterRegisteredEvents.length - voterUnregisteredEvents.length) >= maxVoterperSession) { setErrorMsg("Max voter per session reached"); onOpen(); setAddressToWhitelist(""); return; }
 
 
         if (await contract.methods.addVoter(addressToWhitelist, sessionSelected).call({ from: accounts[0] })) {
@@ -42,18 +48,49 @@ function AdminSessionWhitelist({ sessionSelected, addressToWhitelistLog, setAddr
         }
     };
 
+    //Remove address from whitelist
+    const removeAddressFromWhitelist = async () => {
+        if (!web3.utils.isAddress(addressToRemove)) { setErrorMsg("Address invalid"); onOpen(); setAddressToRemove(""); return; }
+
+        //Validation address already submit
+        const voterRegisteredEvents = await contract.getPastEvents('VoterRegistered', { filter: { voterAddress: addressToWhitelist, sessionID: sessionSelected }, fromBlock: creationBlock, toBlock: 'latest' });
+        const voterUnregisteredEvents = await contract.getPastEvents('VoterUnregistered', { filter: { voterAddress: addressToWhitelist, sessionID: sessionSelected }, fromBlock: creationBlock, toBlock: 'latest' });
+
+        if (voterRegisteredEvents.length === voterUnregisteredEvents.length) { setErrorMsg("Voter not whitelisted"); onOpen(); setAddressToRemove(""); return; }
+
+        if (await contract.methods.removeVoter(addressToRemove, sessionSelected).call({ from: accounts[0] })) {
+            const addAddressTx = await contract.methods.removeVoter(addressToRemove, sessionSelected).send({ from: accounts[0] });
+            const removedAddressToRemove = addAddressTx.events.VoterUnregistered.returnValues.voterAddress;
+            setAddressToWhitelistLog("Address " + removedAddressToRemove + " removed from the Whitelist");
+            setAddressToRemove("");
+        }
+    };
+
     //Build table of  whitelisted addresses
     useEffect(() => {
         (async function () {
-            const voterRegisteredEvents = await contract.getPastEvents('VoterRegistered', { filter: {sessionID: sessionSelected },fromBlock: creationBlock, toBlock: 'latest' });
+            const voterRegisteredEvents = await contract.getPastEvents('VoterRegistered', { filter: { sessionID: sessionSelected }, fromBlock: creationBlock, toBlock: 'latest' });
             const voterAddresses = [];
 
-            for (let i = 0; i < voterRegisteredEvents.length; i++) {
-                voterAddresses.push(
-                    {
-                        blockNumber: voterRegisteredEvents[i].blockNumber,
-                        voterAddress: voterRegisteredEvents[i].returnValues.voterAddress,
-                    });
+            let pos;
+
+            for (let i = (voterRegisteredEvents.length-1); i >= 0; i--) {
+
+                const voterAddedEvents = await contract.getPastEvents('VoterRegistered', { filter: { sessionID: sessionSelected, voterAddress: voterRegisteredEvents[i].returnValues.voterAddress }, fromBlock: creationBlock, toBlock: 'latest' });
+                const voterRemovedEvents = await contract.getPastEvents('VoterUnregistered', { filter: { sessionID: sessionSelected, voterAddress: voterRegisteredEvents[i].returnValues.voterAddress }, fromBlock: creationBlock, toBlock: 'latest' });
+
+                if (voterAddedEvents.length > voterRemovedEvents.length) {
+
+                    pos = voterAddresses.map(e => e.voterAddress).indexOf(voterRegisteredEvents[i].returnValues.voterAddress);
+
+                    if (pos === -1) {
+                        voterAddresses.push(
+                            {
+                                blockNumber: voterRegisteredEvents[i].blockNumber,
+                                voterAddress: voterRegisteredEvents[i].returnValues.voterAddress,
+                            });
+                    }
+                }
             };
 
             //Build table body of registered address
@@ -66,7 +103,7 @@ function AdminSessionWhitelist({ sessionSelected, addressToWhitelistLog, setAddr
 
             setRegisteredAddresses(listAdresses);
         })();
-    }, [contract, accounts, creationBlock,sessionSelected, addressToWhitelistLog])
+    }, [contract, accounts, creationBlock, sessionSelected, addressToWhitelistLog])
 
     return (
         <section className="sessionWhitelist">
@@ -80,9 +117,24 @@ function AdminSessionWhitelist({ sessionSelected, addressToWhitelistLog, setAddr
                                 <FormLabel>Add to whitelist :</FormLabel>
                             </Center>
                             <Spacer />
-                            <Input width='400px' type='text' placeholder="Voter address" onChange={handleAdressChange} value={addressToWhitelist} autoComplete="off" />
+                            <Input width='400px' type='text' placeholder="Voter address" onChange={handleAddressChange} value={addressToWhitelist} autoComplete="off" />
                             <Spacer />
                             <Button colorScheme='gray' onClick={addAddressToWhitelist}>Add Voter</Button>
+                            <Spacer />
+                        </Flex>
+                    </FormControl>
+                </Box>
+                <Box m="25px" >
+                    <FormControl >
+                        <Flex>
+                            <Spacer />
+                            <Center>
+                                <FormLabel>Remove from whitelist :</FormLabel>
+                            </Center>
+                            <Spacer />
+                            <Input width='400px' type='text' placeholder="Voter address" onChange={handleRemoveChange} value={addressToRemove} autoComplete="off" />
+                            <Spacer />
+                            <Button colorScheme='gray' onClick={removeAddressFromWhitelist}>Remove Voter</Button>
                             <Spacer />
                         </Flex>
                     </FormControl>
@@ -92,18 +144,18 @@ function AdminSessionWhitelist({ sessionSelected, addressToWhitelistLog, setAddr
                         <Text></Text>}
                 </Box>
                 <TableContainer my="10px" maxHeight="380px" overflowY="auto">
-                <Table>
-                    <TableCaption>Whitelisted adresses</TableCaption>
-                    <Thead>
-                        <Tr>
-                            <Th>Registration Block Number</Th>
-                            <Th>Address</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>{registeredAddresses}</Tbody>
-                </Table>
-            </TableContainer>
-            </Box>           
+                    <Table>
+                        <TableCaption>Whitelisted adresses</TableCaption>
+                        <Thead>
+                            <Tr>
+                                <Th>Registration Block Number</Th>
+                                <Th>Address</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>{registeredAddresses}</Tbody>
+                    </Table>
+                </TableContainer>
+            </Box>
             <AlertDialog isOpen={isOpen} onClose={onClose} >
                 <AlertDialogOverlay>
                     <AlertDialogContent>
